88cbc41c360ab8712e9c2a59a1fd469ddea2ce9c
José Teles <josetelesmaciel@gmail.com>
2024-05-15 18:17:45 -0300

    refactor: Update import paths and add crawlComicBoom function

--- src/sheets/sheets.ts
+++ src/sheets/sheets.ts
@@ -4,11 +4,11 @@ import credentials from '../../.credentials.json'
 import dotenv from 'dotenv'
 dotenv.config()
 
-export enum SheetsTables {
-    comicboom = 'Comic Boom',
-    comix = 'Comix',
-    qns = 'QnS',
-    scraping = 'Scraping'
+type SheetColumn = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'W' | 'X' | 'Y' | 'Z' | 'AA' | 'AB' | 'AC' | 'AD' | 'AE' | 'AF' | 'AG' | 'AH' | 'AI' | 'AJ' | 'AK' | 'AL' | 'AM' | 'AN' | 'AO' | 'AP' | 'AQ' | 'AR' | 'AS' | 'AT' | 'AU' | 'AV' | 'AW' | 'AX' | 'AY' | 'AZ'
+const alphabet: SheetColumn[] = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'AA', 'AB', 'AC', 'AD', 'AE', 'AF', 'AG', 'AH', 'AI', 'AJ', 'AK', 'AL', 'AM', 'AN', 'AO', 'AP', 'AQ', 'AR', 'AS', 'AT', 'AU', 'AV', 'AW', 'AX', 'AY', 'AZ']
+
+const indexToColumn = (index: number): SheetColumn => {  
+  return alphabet[index]
 }
 
 const auth = new JWT(
@@ -23,11 +23,11 @@ const sheets = google.sheets({ version: 'v4', auth })
 /**
  * Represents the options for inserting data into a sheet.
  */
-interface InsertOptions {
+interface InsertOptions<TableEnum extends string> {
     /**
      * The name of the table where the data will be inserted.
      */
-    tableName?: SheetsTables;
+    tableName?: TableEnum;
 
     /**
      * The range where the data will be inserted.
@@ -37,13 +37,7 @@ interface InsertOptions {
     /**
      * The values to be inserted into the sheet.
      */
-    values: (string | number)[];
-}
-
-interface InsertOptions {
-    tableName?: SheetsTables
-    range: string
-    values: (string|number)[]
+    values: (string | number)[][];
 }
 
 /**
@@ -51,7 +45,7 @@ interface InsertOptions {
  * @param options - The options for inserting values.
  * @throws Error if the SPREADSHEET_ID environment variable is missing.
  */
-async function write(options: InsertOptions) {
+export async function write<TableEnum extends string>(options: InsertOptions<TableEnum>) {
   if(!process.env.SPREADSHEET_ID) {
     throw new Error('Missing SPREADSHEET_ID env variable')
   }
@@ -65,7 +59,7 @@ async function write(options: InsertOptions) {
       range: tableName ? rangeWithSheet : range,
       valueInputOption: 'RAW',
       requestBody: {
-        values: [values]
+        values
       }
     }
 
@@ -76,8 +70,112 @@ async function write(options: InsertOptions) {
   }
 }
 
-void write({
-  tableName: SheetsTables.comicboom,
-  range: 'A1:B1',
-  values: ['Hello', 'World']
-})
+/**
+ * Inserts records into a Google Sheets spreadsheet.
+ * @param options - The options for inserting records.
+ * @throws Error if the SPREADSHEET_ID environment variable is missing.
+ */
+export async function insert<TableEnum extends string, RecordType extends Record<string, any>>(options: { tableName: TableEnum, records: RecordType[] }) {
+  const { tableName, records } = options
+
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: `${tableName}!${alphabet[0]}:${alphabet[alphabet.length - 1]}`
+    })
+
+    if(!response.data.values) {
+      throw new Error('No data found in the sheet.')
+    }
+    const lastLine = response.data.values.length
+    const headers = await getHeaders({ tableName })
+
+    const valuesFromRecords = records.map((record: RecordType) => {
+      const valuesForRow: (string | number)[] = []
+      headers.forEach(header => {
+        const isValidType = typeof record[header.name] === 'string' || typeof record[header.name] === 'number'
+        valuesForRow.push(isValidType ? record[header.name] as string|number : '')
+      })
+      return valuesForRow
+    })
+    const range = `A${lastLine + 1}:${indexToColumn(headers.length - 1)}${lastLine + valuesFromRecords.length}`
+    await write({
+      tableName,
+      range,
+      values: valuesFromRecords
+    })
+    console.log({valuesFromRecords, lastLine, range}) // eslint-disable-line
+  } catch (error) {
+    console.error(`Error inserting data: ${error}`) // eslint-disable-line
+    throw error
+  }
+}
+
+/**
+ * Represents the headers of a sheet.
+ */
+interface SheetHeaders {
+    column: SheetColumn
+    name: string
+    index: number
+} 
+
+/**
+ * Retrieves the headers of a specified table from a Google Sheets document.
+ * @param options - The options for retrieving the headers.
+ * @param options.tableName - The name of the table.
+ * @returns A promise that resolves to an array of SheetHeaders representing the headers of the table.
+ */
+async function getHeaders<TableEnum extends string>(options: {tableName: TableEnum}): Promise<SheetHeaders[]> {
+  const { tableName } = options    
+  try {
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: `${tableName}!1:1`
+    })
+
+    const values = response.data.values
+
+    if (values) {
+      return values[0].map((name: string, index: number) => (
+        { column: indexToColumn(index), name, index }
+      ))
+    } else {
+        console.log('There are no headers in the sheet.') // eslint-disable-line
+      return [] as SheetHeaders[] // Add the return type as SheetHeaders[]
+    }
+  } catch (error) {
+    console.error(`Error getting headers: ${error}`) // eslint-disable-line
+    throw error
+  }
+}
+
+// enum SheetsTables {
+//     comicboom = 'Comic Boom',
+//     comix = 'Comix',
+//     qns = 'QnS',
+//     scraping = 'Scraping'
+// }
+
+// interface ComicBoomTable {
+//     authors: string
+//     title: string
+//     url: string
+//     price: number
+// }
+
+// void insert<SheetsTables, ComicBoomTable>({
+//   tableName: SheetsTables.comicboom,
+//   records: [{
+//     'authors': 'John Doe',
+//     'title': 'Hello World',
+//     'url': 'https://example.com',
+//     'price': 9.99
+//   },
+//   {
+//     'authors': 'Mary Doe',
+//     'title': 'Hello Teles Exemplo 2',
+//     'url': 'https://example2.com',
+//     'price': 0.99
+//   }]
+// })
